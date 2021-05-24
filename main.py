f9ddc65ff6ce893b42aac1d503e2c8570d39fb2b
faroukalsajee <faroukalsajee@gmail.com>
2021-05-23 21:56:25 -0700

    rename file

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
-import assignment2
+import get_data
 
-assignment2.search_by_city_and_bedroom_number("Vancouver", 3)
-assignment2.search_by_city_and_bedroom_number("Burnaby", 3)
-assignment2.search_by_price_range(550000, 1000000)
-assignment2.search_by_city_and_property_type("Burnaby", "Condo")
-assignment2.search_by_city_and_property_type("Vancouver","House")
-assignment2.reduce_price()
-assignment2.search_by_postal_code_range("V", 5)
-assignment2.get_listing("R2510818")
+get_data.search_by_city_and_bedroom_number("Vancouver", 3)
+get_data.search_by_city_and_bedroom_number("Burnaby", 3)
+get_data.search_by_price_range(550000, 1000000)
+get_data.search_by_city_and_property_type("Burnaby", "Condo")
+get_data.search_by_city_and_property_type("Vancouver","House")
+get_data.reduce_price()
+get_data.search_by_postal_code_range("V", 5)
+get_data.get_listing("R2510818")
